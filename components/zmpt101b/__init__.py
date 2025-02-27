d5836ad32b9e0d530cd5e0e3e96a415ccf929f71
idreamshen <idream.shen@gmail.com>
2025-02-27 19:20:23 +0800

    init

---
+++ components/zmpt101b/__init__.py
@@ -0,0 +1,17 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_ID, UNIT_EMPTY, ICON_EMPTY
+
+sensor_ns = cg.esphome_ns.namespace('zmpt101b')
+
+Zmpt101bSensor = sensor_ns.class_('ZMPT101BSensor', cg.PollingComponent)
+
+CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(Zmpt101bSensor)
+})
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
---
+++ components/zmpt101b/zmpt101b_sensor.h
@@ -0,0 +1,33 @@
+#include "esphome.h"
+#include <Arduino.h>
+#include "ZMPT101B.h"
+
+#define SENSITIVITY 509.0f
+
+namespace esphome {
+namespace zmpt101b {
+
+class ZMPT101BSensor : public PollingComponent, public Sensor {
+ public:
+  uint8_t adcPin;
+  ZMPT101B *zmpt101b;
+  // constructor
+  ZMPT101BSensor(uint8_t adcPin) : PollingComponent(1000) {
+    this->adcPin = adcPin;
+    this->zmpt101b = new ZMPT101B(adcPin);
+  }
+
+  float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
+
+  void setup() override {
+    this->zmpt101b->setSensitivity(SENSITIVITY);
+  }
+
+  void update() override {
+    float Vrms = this->zmpt101b->getRmsVoltage();
+    publish_state(Vrms);
+  }
+};
+
+}
+}
