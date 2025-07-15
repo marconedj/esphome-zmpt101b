309d365cd259101f3dbf2a441fe5bc2bc46dd0c0
marconedj <54219901+marconedj@users.noreply.github.com>
2025-07-14 22:08:29 -0300

    Update __init__.py

--- components/zmpt101b/sensor/__init__.py
+++ components/zmpt101b/sensor/__init__.py
@@ -7,11 +7,23 @@ sensor_ns = cg.esphome_ns.namespace('zmpt101b_ns')
 
 Zmpt101bSensor = sensor_ns.class_('ZMPT101BSensor', cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(Zmpt101bSensor),
-    cv.Required("adc_pin"): cv.int_range(min=0, max=99),
-    cv.Optional("sensitivity", default=500): cv.float_range(min=0, max=1000)
-})
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        icon=ICON_FLASH,
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_MEASUREMENT,
+        device_class=DEVICE_CLASS_VOLTAGE,
+    )
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(Zmpt101b),
+            cv.Required(CONF_PIN): pins.internal_gpio_input_pin_schema,
+            cv.Optional(CONF_VOLTAGE, default="3.3"): cv.string,
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID], config["adc_pin"], config["sensitivity"])
